1956ae739a6fe7edb7064b034935223a36fd03c7
Girija Shankar <girijasatyam@gmail.com>
2018-12-30 16:15:51 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DynamicTableComponent } from './dynamic-table/dynamic-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DynamicTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
